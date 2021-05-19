85235a7b8c8d44c39389340a534c913406dc12e7
RNTay <81459529+RNTay@users.noreply.github.com>
2021-05-19 19:20:49 +0800

    Create Hangman.py

---
+++ Hangman.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+answer_string = 'calculator'
+answer = list(answer_string.upper())
+guess = ['_' for _ in answer]
+
+letters = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+wrong_guesses = []
+
+mistakes_made = 0
+
+
+def hangman_drawing(mistakes: int):
+    """Draws the hangman based on the number of mistakes."""
+    if mistakes == 0:
+        drawing = '''
+_______
+|     |
+|
+|
+|
+|_______
+'''
+        print(drawing)
+    elif mistakes == 1:
+        drawing = '''
+_______
+|     |
+|     O
+|
+|
+|_______
+'''
+        print(drawing)
+    elif mistakes == 2:
+        drawing = '''
+_______
+|     |
+|     O
+|     |
+|
+|_______
+'''
+        print(drawing)
+    elif mistakes == 3:
+        drawing = '''
+_______
+|     |
+|     O
+|    /|
+|
+|_______
+'''
+        print(drawing)
+    elif mistakes == 4:
+        drawing = '''
+_______
+|     |
+|     O
+|    /|\\
+|
+|_______
+'''
+        print(drawing)
+    elif mistakes == 5:
+        drawing = '''
+_______
+|     |
+|     O
+|    /|\\
+|    /
+|_______
+'''
+        print(drawing)
+    elif mistakes == 6:
+        drawing = '''
+_______
+|     |
+|     O
+|    /|\\
+|    / \\
+|_______
+'''
+        print(drawing)
+
+
+print('Word:', ''.join(guess))
+while True:  # game is still playing
+    print()
+    guess_letter = input('Guess letter: ').upper()
+    if guess_letter not in letters:
+        print()
+        print('Your guess was not a letter.')
+        print()
+        print('Word:', ''.join(guess))
+        print('Wrong guesses:', ', '.join(wrong_guesses))
+        continue
+
+    if guess_letter in guess:
+        print()
+        print('You already guessed {} and it is correct.'.format(guess_letter))
+        print()
+        print('Word:', ''.join(guess))
+        print('Wrong guesses:', ', '.join(wrong_guesses))
+
+    elif guess_letter in answer:
+        for answer_index in range(len(answer)):
+            if guess_letter == answer[answer_index]:
+                guess[answer_index] = guess_letter
+        print()
+        print('Word:', ''.join(guess))
+        print('Wrong guesses:', ', '.join(wrong_guesses))
+        if guess == answer:
+            print()
+            print('-'*50)
+            print('Congratulations!')
+            print('The word was, indeed, {}.'.format(answer_string.upper()))
+            print('-'*50)
+            break
+    else:
+        if guess_letter in wrong_guesses:
+            print()
+            print('You already guessed {} and it is wrong.'.format(guess_letter))
+            print()
+            print('Word:', ''.join(guess))
+            print('Wrong guesses:', ', '.join(wrong_guesses))
+        else:
+            wrong_guesses.append(guess_letter)
+            mistakes_made += 1
+            hangman_drawing(mistakes_made)
+            print('Word:', ''.join(guess))
+            print('Wrong guesses:', ', '.join(wrong_guesses))
+            if mistakes_made == 6:
+                print()
+                print('-'*50)
+                print('Game over!')
+                print('The word was {}.'.format(answer_string.upper()))
+                print('-'*50)
+                break
